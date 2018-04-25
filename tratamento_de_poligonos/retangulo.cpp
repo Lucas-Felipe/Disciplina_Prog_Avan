ebfdcb1068f78fead182d3a625c5c51493a3a41f
Lucas Felipe <l17.lucas@gmail.com>
2018-04-24 21:11:49 -0300

    Commit pela maquina

--- tratamento_de_poligonos/retangulo.cpp
+++ tratamento_de_poligonos/retangulo.cpp
@@ -3,8 +3,8 @@
 Retangulo::Retangulo(float x, float y, float largura, float altura)
 {
     inserirvertice(x,y);
-    inserirvertice(x+largura,y);
-    inserirvertice(x+largura,y-altura);
     inserirvertice(x,y-altura);
+    inserirvertice(x+largura,y-altura);
+    inserirvertice(x+largura,y);
 }
 
